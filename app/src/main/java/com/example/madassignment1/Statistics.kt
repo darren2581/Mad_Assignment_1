0582dd9174f4e9c04c355b8e6f3c4516ea8d2d75
darren2581 <darrentanthongen@gmail.com>
2024-08-27 20:22:48 +0800

    Add statistics fragment layout

---
+++ app/src/main/java/com/example/madassignment1/Statistics.kt
@@ -0,0 +1,35 @@
+package com.example.madassignment1
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Statistics : Fragment() {
+
+    companion object {
+        fun newInstance() = Statistics()
+    }
+
+    private val viewModel: StatisticsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_statistics, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Initialize your UI elements or set up listeners here
+    }
+}
---
+++ app/src/main/java/com/example/madassignment1/StatisticsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.madassignment1
+
+import androidx.lifecycle.ViewModel
+
+class StatisticsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
