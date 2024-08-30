8f45d52ce690f1d2fcd8725c496bd47d4079e790
darren2581 <darrentanthongen@gmail.com>
2024-08-30 10:55:45 +0800

    Merge Board from nic2

---
+++ app/src/main/java/com/example/madassignment1/Board6x5.kt
@@ -0,0 +1,20 @@
+package com.example.madassignment1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Board6x5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_board6x5)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
